3ffa74662f71202b643c1359e640db74134a157a
Nyx <nyx@mindscape.world>
2016-05-08 22:41:26 +0800

    Actually adds the main file, due to a clerical error the main file was blank.

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    string SplashScreenInText("================\n   Alcor - V\n================\n");
+    cout << SplashScreenInText;
+    return 0;
+}
